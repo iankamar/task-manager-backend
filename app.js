4e6a1f14f7c45ee0a676795f6d60ebcf27777ef4
Ian Kamar <ian.kamar@outlook.com>
2023-12-05 17:47:02 -0500

    update my project

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+// const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+const { PORT = 3001 } = process.env;
+
+// Security headers
+app.use(helmet());
+
+// Rate limit to all requests
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Task Manager API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
