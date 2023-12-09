4ca2cc69c9618fc65a9fe4e0444de0d0a15bc869
Ian Kamar <ian.kamar@outlook.com>
2023-12-08 19:13:27 -0500

    update my project

---
+++ proxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/rest",
+    createProxyMiddleware({
+      target: "https://api.todoist.com",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/rest": "/rest",
+      },
+    })
+  );
+};
