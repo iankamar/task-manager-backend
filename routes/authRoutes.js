25449c1a70f00aa0d7e1cd29fc6776141ed041d2
Ian Kamar <ian.kamar@outlook.com>
2023-12-20 04:26:22 -0500

    update my project

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/register", authController.register);
+router.post(
+  "/register",
+  [
+    check("username").isLength({ min: 1 }).withMessage("Username is required"),
+    check("password").isLength({ min: 1 }).withMessage("Password is required"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    authController.register(req, res);
+    return null;
+  }
+);
 router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.get("/users/me", authMiddleware, authController.getMe);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const taskController = require("../controllers/taskController");
 const authMiddleware = require("../middleware/authMiddleware");
 
@@ -7,7 +8,24 @@ const router = express.Router();
 router.use(authMiddleware);
 
 router.get("/", taskController.getAllTasks);
-router.post("/", taskController.createTask);
+router.post(
+  "/",
+  [
+    check("title").isLength({ min: 1 }).withMessage("Title is required"),
+    check("description")
+      .isLength({ min: 1 })
+      .withMessage("Description is required"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    taskController.createTask(req, res);
+    return null;
+  }
+);
+
 router.get("/:taskId", taskController.getTaskById);
 router.put("/:taskId", taskController.updateTask);
 router.delete("/:taskId", taskController.deleteTask);
