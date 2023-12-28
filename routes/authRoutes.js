92d09c97c3254e4c908d4c29771866870756457d
Ian Kamar <ian.kamar@outlook.com>
2023-12-28 05:37:20 -0500

    update my project

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,25 +8,3 @@ router.post("/signin", authController.signin);
 router.post("/forgot-password", authController.forgotPassword);
 
 module.exports = router;
-
-/*
-const express = require("express");
-const authController = require("../controllers/authController");
-const {
-  registerValidation,
-  loginValidation,
-  forgotPasswordValidation,
-} = require("../middleware/validationMiddleware");
-
-const router = express.Router();
-
-router.post("/signup", registerValidation, authController.signup);
-router.post("/signin", loginValidation, authController.signin);
-router.post(
-  "/forgot-password",
-  forgotPasswordValidation,
-  authController.forgotPassword
-);
-
-module.exports = router;
-*/
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,24 +1,13 @@
 const express = require("express");
 const taskController = require("../controllers/taskController");
 const authMiddleWare = require("../middleware/authMiddleware");
-const validationMiddleware = require("../middleware/validationMiddleware");
 
 const router = express.Router();
 
 router.get("/", authMiddleWare, taskController.getAllTasks);
-router.post(
-  "/",
-  authMiddleWare,
-  validationMiddleware.taskValidation,
-  taskController.createTask
-);
+router.post("/", authMiddleWare, taskController.createTask);
 router.get("/:taskId", authMiddleWare, taskController.getTaskById);
-router.put(
-  "/:taskId",
-  authMiddleWare,
-  validationMiddleware.taskValidation,
-  taskController.updateTask
-);
+router.put("/:taskId", authMiddleWare, taskController.updateTask);
 router.delete("/:taskId", authMiddleWare, taskController.deleteTask);
 
 module.exports = router;
